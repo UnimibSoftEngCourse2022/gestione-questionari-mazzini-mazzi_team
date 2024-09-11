f5084d7eac293c7d53a4e9f88b178747364608ea
andrea mazzini <andrea.mazzini@veycore.com>
2024-09-11 14:41:16 +0200

    feat: add air.go for live reload

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "sas, World!")
-	})
+	e.Use(middleware.Logger())
+	e.Use(middleware.CORS())
+
+	e.GET("/", getQuestions)
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func getQuestions(c echo.Context) error {
+	return c.String(http.StatusOK, "Sas here some questions")
+
 }
